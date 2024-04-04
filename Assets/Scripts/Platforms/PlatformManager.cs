c9994b69fbd5958b8933f5974b078f169b922244
Phillip Stemmlar Schulze <49020766+phillipstemmlar@users.noreply.github.com>
2024-04-04 20:02:53 +0200

    feat: Implemented platform height generation

--- Assets/Scripts/Platforms/PlatformManager.cs
+++ Assets/Scripts/Platforms/PlatformManager.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 public class PlatformManager : MonoBehaviour
 {
 	[Header("Platform Spawning")]
+	[SerializeField] private bool _showDebugLines = true;
 	[SerializeField] private Transform _platformParent;
-	[SerializeField] private Vector2 _platformSize = new Vector2(1.7f, 0.45f);
+	[SerializeField] private float _platformWidth = 1.7f;
+
+	public float platformWidth => _platformWidth;
 
 	[SerializeField] private float _spawnX;
 	[SerializeField] private float _destroyX;
+
 	public float destroyX => _destroyX;
 	public float spawnX => _spawnX;
 
 	[SerializeField] private float _topY;
 	[SerializeField] private float _bottomY;
+	[SerializeField][Range(1, 30)] private int _heightLevels = 13;
+
 	public float topY => _topY;
 	public float bottomY => _bottomY;
 
@@ -29,6 +35,7 @@ public class PlatformManager : MonoBehaviour
 	private List<GameObject> _spawnedPlatforms = new List<GameObject>();
 	private GameObject _latestPlatform;
 	private Vector2 _latestPlatformSize;
+	private int _latestHeightIndex;
 
 	[Header("Platform Movement")]
 	[SerializeField] private float _baseLevelSpeed = 3.0f;
@@ -41,10 +48,18 @@ public class PlatformManager : MonoBehaviour
 	private Vector2 _trackingSize;
 	private float _elapsedTime = 0;
 
+	private float _platformHeight => Mathf.Abs(_topY - _bottomY) / _heightLevels;
+	private Vector2 _platformSize => new Vector2(_platformWidth, _platformHeight);
+
 	private Vector2 _startingPlatformSize => new Vector2(_spawnX - _destroyX, _platformSize.y);
 	private float startingPlatformOffset = 1.0f;
 
 	private DifficultyManager difficultyManager;
+
+	private float _startTime = 0.0f;
+	public float startTime => _startTime;
+
+
 
 	void Start()
 	{
@@ -53,6 +68,9 @@ public class PlatformManager : MonoBehaviour
 		spawnStartingPlatform();
 		_trackingSize = _startingPlatformSize;
 		_elapsedTime = (_trackingSize.x - startingPlatformOffset - _platformSize.x * 0.5f) / _levelSpeed;
+
+		_startTime = Time.time;
+		_latestHeightIndex = 0;
 	}
 
 	void Update()
@@ -79,10 +97,15 @@ public class PlatformManager : MonoBehaviour
 
 	public void spawnRandomPlatform()
 	{
-		int index = Random.Range(0, _platforms.Count);
-		GameObject platform = _platforms[index];
+		int platformIndex = Random.Range(0, _platforms.Count);
+		GameObject platform = _platforms[platformIndex];
 
-		Vector2 position = new Vector2(_spawnX - _levelSpeed * Time.deltaTime, _bottomY);
+		int heightIndex =
+		getRandomHeightIndex(_latestHeightIndex);
+		_latestHeightIndex = heightIndex;
+		float yPos = indexToYPos(heightIndex);
+
+		Vector2 position = new Vector2(_spawnX - _levelSpeed * Time.deltaTime, yPos);
 		spawnPlatform(platform, position, _trackingSize);
 	}
 
@@ -120,31 +143,85 @@ public class PlatformManager : MonoBehaviour
 	public void registerPlatform(GameObject platform) => _spawnedPlatforms.Add(platform);
 	public bool unregisterPlatform(GameObject platform) => _spawnedPlatforms.Remove(platform);
 
+
+	float indexToYPos(int index) => _bottomY + _platformHeight * index;
+	int getRandomHeightIndex(int prevIndex)
+	{
+		float p = Random.value;
+		int diff;
+
+		if (p <= 0.5f) //P(Same Height) = 50%
+		{
+			diff = 0;
+		} else
+		{
+			bool isUp = Random.Range(0, 2) > 0;
+			int size = isUp ? 1 : -1;
+
+			if (p <= 0.8f) // P(Diff 1) = 30%
+			{
+				diff = 1 * size;
+			} else if (p <= 0.95f) // P(Diff 2) = 15%
+			{
+				diff = 2 * size;
+			} else // P(Diff 3) = 5%
+			{
+				diff = 3 * size;
+			}
+		}
+		int index = prevIndex + diff;
+
+		if (index >= _heightLevels || index < 0)
+		{
+			diff *= -1;
+		}
+		index = prevIndex + diff;
+
+		if (index >= _heightLevels) index = _heightLevels - 1;
+		if (index < 0) index = 0;
+
+		return index;
+	}
+
 	private void OnDrawGizmos()
 	{
+		if (!_showDebugLines) return;
+
+		float height = _platformHeight;
+
 		Gizmos.color = Color.green;
 		Gizmos.DrawLine(
-			new Vector3(_spawnX, _topY),
-			new Vector3(_spawnX, _bottomY)
+			new Vector3(_spawnX, _topY - height * 0.5f),
+			new Vector3(_spawnX, _bottomY - height * 0.5f)
 		);
 
 		Gizmos.color = Color.red;
 		Gizmos.DrawLine(
-			new Vector3(_destroyX, _topY),
-			new Vector3(_destroyX, _bottomY)
+			new Vector3(_destroyX, _topY - height * 0.5f),
+			new Vector3(_destroyX, _bottomY - height * 0.5f)
 		);
 
 		Gizmos.color = Color.blue;
 		Gizmos.DrawLine(
-			new Vector3(_spawnX, _topY),
-			new Vector3(_destroyX, _topY)
+			new Vector3(_spawnX, _topY - height * 0.5f),
+			new Vector3(_destroyX, _topY - height * 0.5f)
 		);
 
 		Gizmos.color = Color.blue;
 		Gizmos.DrawLine(
-			new Vector3(_spawnX, _bottomY),
-			new Vector3(_destroyX, _bottomY)
+			new Vector3(_spawnX, _bottomY - height * 0.5f),
+			new Vector3(_destroyX, _bottomY - height * 0.5f)
 		);
+
+		Gizmos.color = Color.yellow;
+		for (int i = 1; i < _heightLevels; ++i)
+		{
+			float y = _bottomY + i * height - height * 0.5f;
+			Gizmos.DrawLine(
+				new Vector3(_spawnX, y),
+				new Vector3(_destroyX, y)
+			);
+		}
 	}
 
 	private void Awake()
